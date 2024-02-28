8dc4c09a12a642cfd61f623f4fbe9a07bb0b61ce
Project_Portfolio <pujantanna009@gmail.com>
2024-02-29 01:31:13 +0530

    Setup the frontend flow, backend flow with the dummy data and creating schema with mongoose

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./dummy-data/data");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Express Server running on the port:${PORT}`);
+});
